c40c119fc2496ff2558d70f3bbf0529d16b249db
Dmitriy Begishev <mintdragon@yandex.ru>
2022-01-12 19:42:49 +0400

    12.01.22

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="")
 
 @app.get("/")
 async def get(request: Request):
