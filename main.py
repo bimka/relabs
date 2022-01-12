a840a3ca02e90670469e7ed73e248d89e477b599
Dmitriy Begishev <mintdragon@yandex.ru>
2022-01-12 18:57:10 +0400

    12.01.22

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from fastapi import FastAPI, HTTPException
+import json
+
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-items = {"foo": "The Foo Wrestlers"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/items-header/{item_id}")
-async def read_item_header(item_id: str):
-    if item_id not in items:
-        raise HTTPException(
-            status_code=404,
-            detail="Item not found",
-            headers={"X-Error": "There goes my error"},
+@app.get("/")
+async def get(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "data": "ola"}
         )
-    return {"item": items[item_id]}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    i = 0
+    while True:
+        i += 1
+        data = await websocket.receive_text()
+        encoded_data = json.loads(data)
+        encoded_data["num"] = i
+        decoded_data = json.dumps(encoded_data)
+        await websocket.send_text(decoded_data)
+
+
+
